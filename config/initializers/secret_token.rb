bdcb01b1f28c5a252d15c2ceec612209f5611bfe
Ronaldo Bahia <bahiamartins@hotmail.com>
2012-01-09 22:45:50 -0200

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Langworldweb::Application.config.secret_token = '8f7f61c7a4ba26b0c7df93291ebd1e1800486ccb9a3bf95b5b2e48d467f44d45fa86f8da802079ff220b747ece2476dae4c15eb55ae3be9f8a2b93a83409dcc5'
