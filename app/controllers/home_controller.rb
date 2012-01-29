991323ae625cf948886ce3ba2b6d2889ef23e56a
João Valente <filipevalente@gmail.com>
2012-01-29 18:26:56 +0000

    Added notification to provide feedback after newsletter subscription. Removed unecessary gems.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,13 @@
 class HomeController < ApplicationController
+
   def index
   end
-  
+
   def save_email
     if !params[:fname].empty?
       ml = MailingList.create(:email => params[:fname])
     end
+    flash[:notice] = "Thank you for subscribing Lang World!"
     redirect_to root_path
   end
 
